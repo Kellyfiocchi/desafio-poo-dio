ddb656daf95079733df3ccdea3004ac4823524d5
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-07-21 11:46:16 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,43 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+   private String titulo;
+   private String descricao;
+   private int cargaHoraria;
+
+  public Curso() {
+
+  }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescrição() {
+        return descricao;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descricao = descrição;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}

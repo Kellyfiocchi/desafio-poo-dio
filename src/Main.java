38b4d1b92f38f96f9383906e6403b1906f299fd7
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-07-21 15:36:55 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -7,12 +9,12 @@ public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
-        curso1.setDescrição(" descricao curso java");
+        curso1.setDescricao(" descricao curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
-        curso2.setDescrição(" descricao curso js");
+        curso2.setDescricao(" descricao curso js");
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
@@ -23,5 +25,32 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNone("Camila");
+        devCamila.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Cobteufos Concluidos Camila: " + devCamila.getConteudosConcluidos());
+
+
+        Dev devJoao = new Dev();
+        devJoao.setNone("Joao");
+        devJoao.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Joao: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Joao: " + devJoao.getConteudosConcluidos());
+
+
+
+
     }
+
+
 }
